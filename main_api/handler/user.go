8e9c7b88b8ecc4aebf41399f5b12d9094137ddda
shin04 <daikon0413@gmail.com>
2021-10-26 17:41:31 +0900

    implement handler

--- main_api/handler/user.go
+++ main_api/handler/user.go
@@ -4,7 +4,6 @@ import (
 	"errors"
 	"log"
 	"net/http"
-	"strconv"
 
 	jwt "github.com/appleboy/gin-jwt/v2"
 	"github.com/jphacks/D_2106_2/repository"
@@ -17,15 +16,15 @@ type UserHandler struct {
 	uc usecase.UserUsecase
 }
 
-type RegisteruserReq struct {
-	Username string
-	Password string
+type RegisterUserRequest struct {
+	Username     string `json:"username"`
+	DeviceId     string `json:"device_id"`
+	Introduction string `json:"introduction"`
 }
 
-// type LoginReq struct {
-// 	Name     string
-// 	Password string
-// }
+type RegisteUserRespose struct {
+	UserId int `json:"user_id"`
+}
 
 func NewUserHandler(userRepo repository.UserRepository) *UserHandler {
 	uc := usecase.UserUsecase{UserRepo: userRepo}
@@ -34,102 +33,47 @@ func NewUserHandler(userRepo repository.UserRepository) *UserHandler {
 }
 
 func (handler *UserHandler) RegisterUser(c *gin.Context, authMiddleware *jwt.GinJWTMiddleware) {
-	userReq := RegisteruserReq{}
-	err := c.ShouldBindJSON(&userReq)
+	req := RegisterUserRequest{}
+	err := c.ShouldBindJSON(&req)
 	if err != nil {
 		log.Print(err)
-		c.JSON(500, gin.H{"err": err.Error()})
+		c.JSON(500, gin.H{"error": err.Error()})
 		return
 	}
 
-	if userReq.Username == "" || userReq.Password == "" {
-		err = errors.New("username or password field is null")
+	if req.Username == "" || req.DeviceId == "" {
+		err = errors.New("username or deviceId field is null")
 		log.Print(err)
-		c.JSON(500, gin.H{"err": err.Error()})
+		c.JSON(500, gin.H{"error": err.Error()})
 		return
 	}
 
-	userId, err := handler.uc.RegisterNewUser(userReq.Username, userReq.Password)
+	userId, err := handler.uc.RegisterNewUser(req.Username, req.DeviceId, req.Introduction)
 	if err != nil {
 		log.Print(err)
-		c.JSON(500, gin.H{"err": err.Error()})
+		c.JSON(500, gin.H{"error": err.Error()})
 		return
 	}
 	if userId == -1 {
 		err = errors.New("register usesr failed")
 		log.Print(err)
-		c.JSON(500, gin.H{"err": err.Error()})
+		c.JSON(500, gin.H{"error": err.Error()})
 		return
 	}
 
-	tokenStr, _, err := authMiddleware.TokenGenerator(userReq.Username)
-	if err != nil {
-		err = errors.New("failed token generate")
-		log.Print(err)
-		c.JSON(500, gin.H{"err": err.Error()})
-		return
-	}
+	response := &RegisteUserRespose{UserId: userId}
 
-	c.JSON(http.StatusOK, gin.H{"userId": userId, "token": tokenStr})
-}
-
-func (handler *UserHandler) GetAllUsers(c *gin.Context) {
-	users, err := handler.uc.GetAllUsers()
-	if err != nil {
-		log.Print(err)
-		c.JSON(500, gin.H{"err": err.Error()})
-		return
-	}
-	if len(users) < 1 {
-		err = errors.New("users not found")
-		log.Print(err)
-		c.JSON(500, gin.H{"err": err.Error()})
-		return
-	}
-
-	c.JSON(http.StatusOK, users)
+	c.JSON(http.StatusOK, gin.H{"data": response})
 }
 
 func (handler *UserHandler) GetUser(c *gin.Context) {
-	idStr := c.Query("id")
-	userId, err := strconv.Atoi(idStr)
+	deviceId := c.Query("device_id")
+	user, err := handler.uc.GetUserByDeviceId(deviceId)
 	if err != nil {
 		log.Print(err)
-		c.JSON(500, gin.H{"err": err.Error()})
-		return
-	}
-	user, err := handler.uc.GetUserById(userId)
-	if err != nil {
-		log.Print(err)
-		c.JSON(500, gin.H{"err": err.Error()})
+		c.JSON(500, gin.H{"error": err.Error()})
 		return
 	}
 
-	c.JSON(http.StatusOK, user)
+	c.JSON(http.StatusOK, gin.H{"data": user})
 }
-
-// func (handler *UserHandler) Login(c *gin.Context) {
-// 	req := LoginReq{}
-// 	err := c.ShouldBindJSON(&req)
-// 	if err != nil {
-// 		log.Print(err)
-// 		c.JSON(500, gin.H{"err": err.Error()})
-// 		return
-// 	}
-
-// 	if req.Name == "" || req.Password == "" {
-// 		err = errors.New("username or password field not null")
-// 		log.Print(err)
-// 		c.JSON(500, gin.H{"err": err.Error()})
-// 		return
-// 	}
-
-// 	user, err := handler.uc.Login(req.Name, req.Password)
-// 	if err != nil {
-// 		log.Print(err)
-// 		c.JSON(500, gin.H{"err": err.Error()})
-// 		return
-// 	}
-
-// 	c.JSON(http.StatusOK, gin.H{"userId": user.Id})
-// }

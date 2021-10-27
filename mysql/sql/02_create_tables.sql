658bf1f5b79a924a8a015a814f08a1af407e78c3
shin04 <daikon0413@gmail.com>
2021-10-27 12:12:22 +0900

    fix init sql

--- mysql/sql/02_create_tables.sql
+++ mysql/sql/02_create_tables.sql
@@ -26,11 +26,10 @@ CREATE TABLE IF NOT EXISTS images (
 );
 
 CREATE TABLE IF NOT EXISTS users (
-    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    device_id VARCHAR(256),
-    name VARCHAR(128),
-    profile_image_url VARCHAR(128),
-    introduction VARCHAR(128)
+    id VARCHAR(256) NOT NULL PRIMARY KEY,
+    name VARCHAR(128) NOT NULL,
+    profile_image_url VARCHAR(128) NOT NULL,
+    introduction VARCHAR(128) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS friends (
